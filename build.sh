f42ecfc231bcf717ee4b14743b25544191ce963f
Hemanth Kumar C <hemanthkumarc04@gmail.com>
2026-03-13 22:55:22 +0530

    Enforce PostgreSQL on Render and add build debugging

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+if [ -z "$DATABASE_URL" ]; then
+    echo "WARNING: DATABASE_URL is not set. Falling back to SQLite."
+else
+    echo "DATABASE_URL is set (PostgreSQL connection available)."
+fi
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
